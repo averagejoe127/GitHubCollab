c0aca90d4de2db9fadc32a0760ae66c535f54edf
Tim Breon <timbreon@Tims-MacBook-Air.local>
2019-04-23 14:37:48 -0400

    Made changes in vi

--- GitHubCollab/src/App.java
+++ GitHubCollab/src/App.java
@@ -2,6 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+	System.out.println("This line of code was added through terminal");
     }
     
 }
