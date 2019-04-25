eaa999d62385b9a005f026a7162e25fbf26ddbf9
Tim Breon <timbreon@Tims-MacBook-Air.local>
2019-04-25 13:46:01 -0400

    added another line of code

--- GitHubCollab/src/App.java
+++ GitHubCollab/src/App.java
@@ -3,6 +3,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	System.out.println("This line of code was added through terminal");
+	System.out.println("This is another change");
     }
     
 }
